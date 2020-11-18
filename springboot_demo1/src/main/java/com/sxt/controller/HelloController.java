c8d68ce9c1cc0e920ec8eed31dceed8e289c5196
zhangsan <zhangsan@163.com>
2020-11-19 00:20:37 +0800

    修改了hello1()方法

--- springboot_demo1/src/main/java/com/sxt/controller/HelloController.java
+++ springboot_demo1/src/main/java/com/sxt/controller/HelloController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("hello1")
     public String hello1(){
+        System.out.println("this is a test!");
         return "Hello springboot，我来了！";
     }
     @GetMapping("hello2")
