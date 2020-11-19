cd3a1b2af1503adcf3ad05508f7b47d772061106
zhangsan <zhangsan@163.com>
2020-11-19 14:05:18 +0800

    1111111

--- springboot_demo1/src/main/java/com/sxt/Demo1Application.java
+++ springboot_demo1/src/main/java/com/sxt/Demo1Application.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo1Application {
     public static void main(String[] args) {
         SpringApplication.run(Demo1Application.class);
+        System.out.println("1111111111111111111111");
     }
 }
